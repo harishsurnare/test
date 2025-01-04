1554981d1be506b55b2460f74a7919daf5c4c1f7
harish surnare <harishsurnare2002@gmail.com>
2025-01-04 20:46:49 +0530

    erased

--- git_test.java
+++ git_test.java
@@ -2,10 +2,7 @@ package test;
 
 public class git_test {
     public static void main(String[] args) {
-        System.out.println("welcome home guys");
-        System.out.println("hope everyone is fine");
-        System.out.println("howdy dudegit s");
-        System.out.println("this is master branch");
-        System.out.println("this is child branch");
+        System.out.println("everything erasedgit ");
+
     }
 }
