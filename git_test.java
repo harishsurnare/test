fdc1d543f21899760f64e18b7f3b6e118f079120
harish surnare <harishsurnare2002@gmail.com>
2025-01-04 12:38:06 +0530

    Revert "howdy"
    This reverts commit 1edf686b60a1bec60785d3b61e9580d4a1d8fb3f.

--- git_test.java
+++ git_test.java
@@ -4,6 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("welcome home guys");
         System.out.println("hope everyone is fine");
-        System.out.println("howdy dudegit s");
+        System.out.println("howdy dude");
     }
 }
